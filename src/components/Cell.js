6ddb95676e8f51d2e2c7d5f681f15f41745fe140
Ivan Opar <vanya.opar@gmail.com>
2024-10-28 22:26:02 +0200

    2048 game debiut

---
+++ src/components/Cell.js
@@ -0,0 +1,23 @@
+// import React from "react";
+
+
+// export default function Cell() {
+
+
+//     return (
+//         <div className='cell'></div>
+//     )
+// }
+
+import React from "react";
+
+
+const Cell = () => {
+
+
+    return (
+        <div className='cell'></div>
+    )
+}
+
+export default Cell;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import './main.scss';
+import './styles.scss';
+import BoardView from './components/Board';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+    return <BoardView />;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
+// import React from "react";
+// import ReactDOM from "react-dom"
+// import BoardView from './components/Board';
+// import './main.scss';
+// import './styles.scss';
+
+// const App = () => {
+//     return <BoardView />;
+// };
+
+// ReactDOM.render(<App />, document.getElementById('root'));
