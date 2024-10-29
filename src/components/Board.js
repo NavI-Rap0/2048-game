1fcc312128cfd9087576b91a62e7d7951a3577b5
Ivan Opar <vanya.opar@gmail.com>
2024-10-29 16:20:48 +0200

    fixed

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import Tile from "./Tile";
 import Cell from "./Cell";
 import { Board } from "../helper";
@@ -7,55 +7,58 @@ import useSwipe from "../hooks/useSwipe";
 import GameOverlay from "./GameOverlay";
 
 const BoardView = () => {
-  const [board, setBoard] = useState(new Board());
+  const [board, setBoard] = useState(() => new Board());
 
-  const handleMove = (direction) => {
-    if (board.hasWon()) {
-      return;
-    }
-    let boardClone = Object.assign(Object.create(Object.getPrototypeOf(board)), board);
-    let newBoard = boardClone.move(direction);
+  const handleMove = useCallback((direction) => {
+    if (board.hasWon()) return;
+
+    const boardClone = Object.assign(Object.create(Object.getPrototypeOf(board)), board);
+    const newBoard = boardClone.move(direction);
     setBoard(newBoard);
-  };
+  }, [board]);
 
-  const handleKeyDown = (event) => {
+  const handleKeyDown = useCallback((event) => {
     if (event.keyCode >= 37 && event.keyCode <= 40) {
-      let direction = event.keyCode - 37;
+      const direction = event.keyCode - 37;
       handleMove(direction);
     }
-  };
+  }, [handleMove]);
 
   useEvent("keydown", handleKeyDown);
 
-  useSwipe((swipeDirection) => {
-    const directions = {
-      up: 1,
-      right: 2,
-      down: 3,
-      left: 0,
-    };
-    if (directions[swipeDirection] !== undefined) {
-      handleMove(directions[swipeDirection]);
-    }
-  });
+  useSwipe(
+    useCallback((swipeDirection) => {
+      const directions = {
+        up: 1,
+        right: 2,
+        down: 3,
+        left: 0,
+      };
+      if (directions[swipeDirection] !== undefined) {
+        handleMove(directions[swipeDirection]);
+      }
+    }, [handleMove])
+  );
 
-  const resetGame = () => {
-    setBoard(new Board());
-  };
+  const resetGame = useCallback(() => {
+    setBoard(() => new Board());
+  }, []);
 
-  const cells = board.cells.map((row, rowIndex) => {
-    return (
+  const cells = useMemo(() => (
+    board.cells.map((row, rowIndex) => (
       <div key={rowIndex}>
         {row.map((col, colIndex) => (
           <Cell key={rowIndex * board.size + colIndex} />
         ))}
       </div>
-    );
-  });
+    ))
+  ), [board.cells, board.size]);
 
-  const tiles = board.tiles
-    .filter((tile) => tile.value !== 0)
-    .map((tile, index) => <Tile tile={tile} key={index} />);
+  const tiles = useMemo(() => (
+    board.tiles
+      .filter((tile) => tile.value !== 0)
+      .map((tile, index) => <Tile tile={tile} key={index} />)
+  ), [board.tiles]);
 
   return (
     <div>
